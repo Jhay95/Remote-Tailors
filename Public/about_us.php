8f84baabaacd099d3a2a654560bfe8dc1fc05abd
jhay95 <missjay2805@gmail.com>
2021-02-15 19:12:43 +0000

    About us updated

--- Public/about_us.php
+++ Public/about_us.php
@@ -1,12 +1,71 @@
 <?php
 require_once('head.php');
-require_once('navigation.php');
 ?>
+<!--Carousel/Banner ---->
+<header>
+    <?php
+    require_once("navigation.php");
+    ?>
+
+    <div class="banner">
+
+    </div>
+</header>
 
 <section>
-    <h3>About Us!!</h3>
-</section>
+    <div class="container">
+        <h3>About Us!!</h3>
 
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            magna
+            aliqua. Ipsum suspendisse ultrices gravida dictum. Amet purus gravida quis blandit turpis. Mattis molestie a
+            iaculis
+            at erat pellentesque adipiscing commodo. Eget aliquet nibh praesent tristique magna sit amet purus. Aliquam
+            sem
+            fringilla ut morbi tincidunt augue. Risus pretium quam vulputate dignissim suspendisse in est. Dictum sit
+            amet
+            justo
+            donec enim diam vulputate. Mattis nunc sed blandit libero volutpat sed cras ornare. Hac habitasse platea
+            dictumst
+            quisque sagittis purus. Volutpat sed cras ornare arcu dui vivamus arcu felis. Augue ut lectus arcu bibendum
+            at
+            varius vel. Egestas integer eget aliquet nibh. Tristique senectus et netus et malesuada fames ac turpis
+            egestas.
+            Donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Morbi enim nunc faucibus a
+            pellentesque
+            sit
+            amet. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas.
+        </p>
+
+        <p>
+            Dignissim cras tincidunt lobortis feugiat vivamus at augue. Tempus quam pellentesque nec nam aliquam sem et
+            tortor
+            consequat. Amet porttitor eget dolor morbi non arcu risus quis. Placerat duis ultricies lacus sed turpis
+            tincidunt
+            id aliquet risus. Quam quisque id diam vel. Aliquam purus sit amet luctus venenatis lectus. Ultricies
+            integer
+            quis
+            auctor elit sed vulputate mi. Proin sagittis nisl rhoncus mattis. Et molestie ac feugiat sed lectus.
+            Elementum
+            sagittis vitae et leo duis ut diam quam nulla.
+        </p>
+
+        <p>
+            Ultrices vitae auctor eu augue ut lectus arcu bibendum. Venenatis lectus magna fringilla urna porttitor
+            rhoncus
+            dolor purus non. Id aliquet lectus proin nibh nisl condimentum id venenatis a. Enim lobortis scelerisque
+            fermentum
+            dui faucibus in ornare quam. Quis commodo odio aenean sed. Ultrices gravida dictum fusce ut. Sagittis eu
+            volutpat
+            odio facilisis mauris. Praesent semper feugiat nibh sed pulvinar. Sed tempus urna et pharetra pharetra massa
+            massa
+            ultricies. Suspendisse in est ante in nibh mauris cursus mattis molestie. Nec ullamcorper sit amet risus
+            nullam
+            eget
+            felis eget nunc. Facilisis sed odio morbi quis commodo odio aenean sed adipiscing.
+        </p>
+    </div>
+</section>
 
 
 <?php
