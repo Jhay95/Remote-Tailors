d913a5c72414ad81d1e3d53935f758eba1fe5559
jhay95 <missjay2805@gmail.com>
2021-02-15 19:16:33 +0000

    New pages added

---
+++ Public/men.php
@@ -0,0 +1,58 @@
+<?php
+require_once('../Private/functions.php');
+
+// Test tailor data
+$res = get_tailor();
+?>
+
+<!--HTML !Doc Starts here-->
+<?php
+require_once('head.php');
+?>
+
+
+<!--Carousel/Banner ---->
+<header>
+    <?php
+    require_once("navigation.php");
+    ?>
+
+    <div class="banner">
+
+    </div>
+</header>
+
+<!--- Filter bar ---->
+<div class="jumbotron">
+
+</div>
+
+
+<!---Display Table--->
+<section class="container">
+    <h1>Welcome!!</h1>
+
+    <div class="container">
+        <div class="row">
+            <?php while ($row = $res->fetch_assoc()) { ?>
+                <div class="col-sm-4 display">
+                    <div>
+                        <img class="pics" src="" alt="">
+                    </div>
+
+                    <div>
+                        <?php echo "<h6>" . $row["tailor_fname"]. " " . $row["tailor_lname"] . "</h6>". "<br>"; ?>
+                    </div>
+
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+
+
+</section>
+
+
+<?php
+require_once('footer.php');
+?>
