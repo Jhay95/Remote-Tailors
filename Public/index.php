50c80b68d21f3010cfe0c8a63ca56ff0849ce07d
jhay95 <missjay2805@gmail.com>
2021-02-15 19:15:32 +0000

    Banner class added to header to replace carousel

--- Public/index.php
+++ Public/index.php
@@ -3,7 +3,6 @@ require_once('../Private/functions.php');
 
 // Test tailor data
 $res = get_tailor();
-
 ?>
 
 <!--HTML !Doc Starts here-->
@@ -18,46 +17,8 @@ require_once('head.php');
     require_once("navigation.php");
     ?>
 
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="d-block w-100" src="../assets/images/pexels-cottonbro-4620607.jpg" alt="First slide">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>...</h5>
-                    <p>...</p>
-                </div>
-            </div>
+    <div class="banner">
 
-            <div class="carousel-item">
-                <img class="d-block w-100" src="../assets/images/pexels-cottonbro-4620610.jpg" alt="Second slide">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>...</h5>
-                    <p>...</p>
-                </div>
-            </div>
-
-            <div class="carousel-item">
-                <img class="d-block w-100" src="../assets/images/pexels-pixabay-461035.jpg" alt="Third slide">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>...</h5>
-                    <p>...</p>
-                </div>
-            </div>
-
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
     </div>
 </header>
 
@@ -81,9 +42,7 @@ require_once('head.php');
 
                     <div>
                         <?php echo "<h6>" . $row["tailor_fname"]. " " . $row["tailor_lname"] . "</h6>". "<br>"; ?>
-
                     </div>
-
 
                 </div>
             <?php } ?>
