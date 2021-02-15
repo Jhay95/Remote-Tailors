82989817d80c0b9bac8ce51156e53dc3beecfd68
jhay95 <missjay2805@gmail.com>
2021-02-15 23:22:13 +0000

    Form centralized

--- Public/login.php
+++ Public/login.php
@@ -1,8 +1,6 @@
-<!--HTML !Doc Starts here-->
 <?php
 require_once('head.php');
 ?>
-
 
 <!--Carousel/Banner ---->
 <header>
@@ -15,58 +13,72 @@ require_once('head.php');
     </div>
 </header>
 
-<section>
-
+<section class="content">
     <div class="container">
-        <h2>Login Here!</h2>
-        <nav>
-            <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                <a class="nav-item nav-link active" id="nav-tailor-tab" data-toggle="tab" href="#nav-tailor" role="tab"
-                   aria-controls="nav-tailor" aria-selected="true">Tailor</a>
-                <a class="nav-item nav-link" id="nav-customer-tab" data-toggle="tab" href="#nav-customer" role="tab"
-                   aria-controls="nav-customer" aria-selected="false">Customer</a>
-            </div>
-        </nav>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <h2>Create Account</h2>
 
-        <div class="tab-content" id="nav-tabContent">
-            <div class="tab-pane fade show active" id="nav-tailor" role="tabpanel" aria-labelledby="nav-tailor-tab">
-                <form class="container" action="../Private/functions.php" method="post">
+                <div>
+                    <nav>
+                        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                            <a class="nav-item nav-link active" id="nav-tailor-tab" data-toggle="tab" href="#nav-tailor"
+                               role="tab"
+                               aria-controls="nav-tailor" aria-selected="true">Tailor</a>
+                            <a class="nav-item nav-link" id="nav-customer-tab" data-toggle="tab" href="#nav-customer"
+                               role="tab"
+                               aria-controls="nav-customer" aria-selected="false">Customer</a>
+                        </div>
+                    </nav>
 
-                    <div class="form-group">
-                        <label for="uname">Username</label>
-                        <input type="text" class="form-control" id="uname">
+                    <div class="tab-content" id="nav-tabContent">
+                        <div class="tab-pane fade show active" id="nav-tailor" role="tabpanel"
+                             aria-labelledby="nav-tailor-tab">
+                            <form class="container" action="../Private/functions.php" method="post">
+                                <div class="form-group">
+                                    <label for="uname">Username</label>
+                                    <input type="text" class="form-control" id="uname">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="email">Email address</label>
+                                    <input type="email" class="form-control" id="email">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input type="password" class="form-control" id="password">
+                                </div>
+
+                                <button type="submit" class="btn btn-primary" name="submit-tailor">Submit</button>
+                            </form>
+                        </div>
+
+                        <div class="tab-pane fade show" id="nav-customer" role="tabpanel"
+                             aria-labelledby="nav-customer-tab">
+                            <form class="container" action="../Private/functions.php" method="post">
+
+                                <div class="form-group">
+                                    <label for="email">Email address</label>
+                                    <input type="email" class="form-control" id="email">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input type="password" class="form-control" id="password">
+                                </div>
+
+                                <button type="submit" class="btn btn-primary" name="submit-visitor">Submit</button>
+                            </form>
+
+                        </div>
+
                     </div>
-
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control" id="password">
-                    </div>
-
-                    <button type="submit" class="btn btn-primary" name="submit-tailor">Login</button>
-                </form>
-            </div>
-
-            <div class="tab-pane fade show active" id="nav-customer" role="tabpanel" aria-labelledby="nav-customer-tab">
-                <form class="container" action="../Private/functions.php" method="post">
-
-                    <div class="form-group">
-                        <label for="uname">Username</label>
-                        <input type="text" class="form-control" id="uname">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control" id="password">
-                    </div>
-
-                    <button type="submit" class="btn btn-primary" name="submit-customer">Login</button>
-
-                </form>
+                </div>
             </div>
         </div>
     </div>
 </section>
-
 <?php
 require_once('footer.php');
 ?>
