1b16da068fb7266b4942790afe01ecc8ed2ec5de
jhay95 <missjay2805@gmail.com>
2021-02-17 04:24:10 +0000

    Session and Logout button added

--- Public/tailors/index.php
+++ Public/tailors/index.php
@@ -1,3 +1,17 @@
+<?php
+require_once('../../Private/initialize.php');
+session_start();					//retrieve or create session
+$www   = WWW_ROOT;
+$unauth = 'login.php';
+
+if (!IsSet($_SESSION["user"]))			//user name must in session to stay here
+{
+    header("Location: $www$unauth");		//if not, go back to login page
+    exit();
+}
+$username=$_SESSION["user"];		//get user name into variable $username
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -9,5 +23,8 @@
 </head>
 <body>
 <h1>Tailors Dashboard: Coming Soon!!</h1>
+<form method="get" action="../logout.php">
+    <input type="submit" name="logout">
+</form>
 </body>
 </html>
