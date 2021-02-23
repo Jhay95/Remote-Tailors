b883ebb2c2a8017968d6184aad7dc83f49bd63ef
jhay95 <missjay2805@gmail.com>
2021-02-23 01:46:56 +0000

    Action script for login validation

--- Private/validate.php
+++ Private/validate.php
@@ -1,67 +1,32 @@
+<!--
+https://www.onlineittuts.com/login-form-in-php-with-session-and-mysql.html
+This script receives a post request from the login form and carries out the following
+1. checks that the form fields are not empty
+2. checks that the form entries are valid with the database record
+3. redirect valid login to dashboard
+-->
+
 <?php
-// Start Session
-session_start();
-
 require_once('initialize.php');
-require_once("functions.php");
 
-$www = WWW_ROOT;
-$auth = 'tailors/index.php';      //use URL to if access is granted
-$unauth = 'login.php';              // use URL is access is not granted
+session_start(); // Start Session
 
-// Check for $_POST array if none, destroy session and redirect user to home page
-if (!IsSet($_POST))
-{
-    session_destroy();
-    header("Location: $www$unauth");
-    exit();
-}
+$auth = WWW_ROOT . "tailors/index.php";    // use URL if access is granted
+$unauth = WWW_ROOT . "login.php";          // use URL if access is not granted
 
-// Check if username is empty
-if(empty($_POST["email"])){
-    echo "Please enter your email address.";
-}
-
-// Check if password is empty
-if(empty($_POST["password"])){
-    echo "Please enter your Password.";
-}
-
-// If no username and password set, redirect to login page
-if (!IsSet($_POST["email"]) || !IsSet($_POST["password"]))
-{
-    session_destroy();
-    header("Location: $www$unauth");
-    exit();
-}
-
-
-
-// function to validate login details
-function login_validate($email,$password): mysqli_result|bool
-{
-    // Check to see if details exist in database
-    if ($_POST["submit-tailor"]) {
-        $sql = "SELECT `tailor_id` FROM `tailors` WHERE `tailor_email` = '" . $email . "' and `tailor_password` = '" . $password . "'";
+if (isset($_POST)) {                        // Check if there was is a post request
+    // If no username and password set, redirect to login page
+    if (empty($_POST["email"]) || empty($_POST["password"])) {
+        header("Location: $unauth?Empty=Please fill in the blank fields!!");
+        exit();
+    } else {                //validate login with database record
+        $result = login_validate($_POST["email"], $_POST["password"]);
+        if ($result->fetch_assoc()) {               // if a value is returned from the function login_validate
+            $_SESSION["user"] = $_POST["email"];    //store email into session variable
+            header("Location: $auth");       //then forward to home page
+            exit();							        //stop PHP script here
+        } else {                                    // If login details are incorrect
+            header("Location: $unauth?Invalid=Please enter correct login details!!");
+        }
     }
-    $conn = db_connect();
-    return $conn->query($sql);
 }
-
-$email= $_POST["email"];			//get username value submitted by form
-$password= $_POST["password"];		    //get password from form
-
-$result = login_validate($email,$password);
-print_r($result);
-
-//validate login
-if ($result == 1)
-{
-    $_SESSION["user"]=$email;	//store username into session variable
-    header("Location: $www$auth");
-    //header("Location: index.php");//then forward to home page
-    exit();							//stop PHP script here
-}
-
-session_destroy();					//destroy the session
-header("Location: $www$unauth");
