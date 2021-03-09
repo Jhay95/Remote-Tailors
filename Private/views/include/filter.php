4c73948558136593069e752e9fe61f6305458e72
jhay95 <missjay2805@gmail.com>
2021-03-09 23:17:37 +0000

    some styling adjustments

--- Private/views/include/filter.php
+++ Private/views/include/filter.php
@@ -1,25 +1,25 @@
 <div class="filter">
     <nav class="navbar navbar-expand-sm justify-content-around">
         <form>
-            <label for="city" class="form-label">City: </label>
+            <label for="city" class="form-label">City: .<a href="<?= URL_ROOT ?>tailors/city"></a></label>
             <select id="city" class="form-select">
-                <option selected>Choose...</option>
-                <!---Get list of city from database---->
-                <option>...</option>
+               <!---Get list of city from database---->
+                <?php foreach($data['city'] as $city): ?>
+                <option><?php echo $city;?>></option>
+                <?php endforeach;?>
             </select>
         </form>
 
         <form>
             <label for="style" class="form-label">Style: </label>
             <select id="style" class="form-select">
-                <option selected>Choose...</option>
                 <option>English</option>
                 <option>Native</option>
                 <option>Casual</option>
             </select>
         </form>
 
-        <form class="form-inline" action="/action_page.php">
+        <form class="form-inline" action="<?= URL_ROOT ?>tailors/search">
             <input class="form-control mr-sm-2" type="text" placeholder="Search">
             <button class="btn btn-secondary" type="submit">Search</button>
         </form>
