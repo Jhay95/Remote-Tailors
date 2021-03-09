4a4852f2a98eaf9b1e73e9bc475ed171466af478
jhay95 <missjay2805@gmail.com>
2021-03-09 23:12:18 +0000

    customers login form updated

--- Private/views/customers/login.php
+++ Private/views/customers/login.php
@@ -35,15 +35,21 @@ require_once(INC_PATH . 'head.php');
                 <h2>Sign in</h2>
 
                 <div>
-                    <form class="container" action="<?= URL_ROOT ?>customers/login" method="post">
+                    <form class="container" action="<?= URL_ROOT ?>customers/signin" method="post">
                         <div class="form-group">
                             <label for="email">Email address</label>
-                            <input type="email" class="form-control" name="email">
+                            <input type="email"
+                                   class="form-control <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>"
+                                   name="email" value="<?php echo $data['email']; ?>">
+                            <span class="invalid-feedback"><?php echo $data['email_err']; ?></span>
                         </div>
 
                         <div class="form-group">
                             <label for="password">Password</label>
-                            <input type="password" class="form-control" name="password">
+                            <input type="password"
+                                   class="form-control <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>"
+                                   name="password" value="<?php echo $data['password']; ?>">
+                            <span class="invalid-feedback"><?php echo $data['password_err']; ?></span>
                         </div>
                         <input type="submit" class="btn btn-primary" name="submit-visitor">
                     </form>
