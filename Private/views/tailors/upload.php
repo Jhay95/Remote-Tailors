a4c6cefcf27b2e7c357e7ca18620b154bd03115f
clintz1 <clintz2009@gmail.com>
2021-04-12 08:38:42 +0100

    Update upload.php

--- Private/views/tailors/upload.php
+++ Private/views/tailors/upload.php
@@ -2,12 +2,14 @@
 
 <?php require_once(INC_PATH . 'navigation.php'); ?>
 
+<?php echo "Hello" ?>;
+
 <section class="container">
     <div class="container p-5 d-flex justify-content-center">
         <div class="justify-content-center">
             <h2>Upload Photos</h2>
             <div>
-                <form class="row g-3 needs-validation" action="<?= URL_ROOT ?>Profiles/upload/<?=$data['id']?>"
+                <form class="row g-3 needs-validation" action="<?= URL_ROOT ?>Profiles/upload.php/<?=$data['id']?>"
                       method="post">                      
                     Select Image File to Upload:
                    <input type="file" name="images" multiple="multiple">
