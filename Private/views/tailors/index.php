8b920c7f77d01744cec857518ca595d5e2438796
jhay95 <missjay2805@gmail.com>
2021-04-19 16:14:41 +0100

    tailors profile index updated

--- Private/views/tailors/index.php
+++ Private/views/tailors/index.php
@@ -11,7 +11,19 @@
                     <!--Profile pics--->
                     <div class="col-md-3">
                         <div class="card-body">
-                            <img src="http://via.placeholder.com/180x180" alt="Card image">
+                            <?php if (empty($data['photo'])): ?>
+                                <?php if ($data['tailor']['tailor_gender'] == 'Male'): ?>
+                                    <img src="<?php echo URL_ROOT; ?>assets/profile_uploads/m dummy.png"
+                                         alt="Card image" width="160px" height="160px">
+                                <?php else: ?>
+                                    <img src="<?php echo URL_ROOT; ?>assets/profile_uploads/f dummy.png"
+                                         alt="Card image" width="160px" height="160px">
+                                <?php endif; ?>
+
+                            <?php else: ?>
+                                <img src="<?php echo URL_ROOT; ?>assets/profile_uploads/<?php echo $data['photo']['photo_name']; ?>"
+                                     alt="Card image" width="160px" height="160px">
+                            <?php endif; ?>
                         </div>
                     </div>
 
@@ -31,11 +43,14 @@
                                 <h6 class="card-text"><strong>STYLE: </strong>
                                     <em><?php echo $data['tailor']['tailor_style']; ?></em></h6>
                                 <br>
-                                <h6 class="card-text"><strong>GENDER PREFERENCE: </strong>
+                                <h6 class="card-text"><strong>SOWS for: </strong>
                                     <em><?php echo $data['tailor']['tailor_pref']; ?></em></h6>
                             </div>
 
                             <div class="col-md-6">
+                                <br>
+                                <h6 class="card-text">
+                                    <strong>GENDER: </strong><em><?php echo $data['tailor']['tailor_gender']; ?></em></h6>
                                 <br>
                                 <h6 class="card-text">
                                     <strong>EMAIL: </strong><em><?php echo $data['tailor']['tailor_email']; ?></em></h6>
@@ -52,11 +67,17 @@
             <div class="col-md-3">
                 <div class="well well-sm text-center">
                     <div class="btn-group-vertical">
-                        <button type="button" class="btn btn-secondary"><a href="<?php echo URL_ROOT; ?>profiles/edit/<?php echo $data['tailor']['tailor_id'];?>">Edit Profile</a></button>
+                        <button type="button" class="btn btn-secondary"><a
+                                    href="<?php echo URL_ROOT; ?>profiles/edit/<?php echo $data['tailor']['tailor_id']; ?>">Edit
+                                Profile</a></button>
                         <br>
-                        <button type="button" class="btn btn-secondary"><a href="<?= URL_ROOT ?>profiles/upload/<?= $data['tailor']['tailor_id']?>">Upload Photo</a></button>
+                        <button type="button" class="btn btn-secondary"><a
+                                    href="<?php echo URL_ROOT; ?>profiles/upload/<?php echo $data['tailor']['tailor_id']; ?>">Upload
+                                Photo</a></button>
                         <br>
-                        <button type="button" class="btn btn-secondary">Upload Works</button>
+                        <button type="button" class="btn btn-secondary"><a
+                                    href="<?php echo URL_ROOT; ?>profiles/works/<?php echo $data['tailor']['tailor_id']; ?>">Upload
+                                Works</a></button>
                     </div>
                 </div>
             </div>
@@ -72,22 +93,18 @@
             <div class="col-md-9">
                 <h3>Tailor's Works</h3>
                 <div class="row">
-                    <div class="col-sm-3">
-                        <img src="http://via.placeholder.com/140x140" class="img-responsive" style="width:100%"
-                             alt="Image">
-                    </div>
-                    <div class="col-sm-3">
-                        <img src="http://via.placeholder.com/140x140" class="img-responsive" style="width:100%"
-                             alt="Image">
-                    </div>
-                    <div class="col-sm-3">
-                        <img src="http://via.placeholder.com/140x140" class="img-responsive" style="width:100%"
-                             alt="Image">
-                    </div>
-                    <div class="col-sm-3">
-                        <img src="http://via.placeholder.com/140x140" class="img-responsive" style="width:100%"
-                             alt="Image">
-                    </div>
+                    <?php if (empty($data['works'])): ?>
+                        <div class="col-sm-3">
+                            <img src="http://via.placeholder.com/180x180" alt="Card image">
+                        </div>
+                    <?php else: ?>
+                        <?php foreach ($data['works'] as $work): ?>
+                            <div class="col-sm-3">
+                                <img src="<?php echo URL_ROOT; ?>assets/work_uploads/<?php echo $work['photo_name']; ?>"
+                                     alt="Card image" width="180px" height="180px">
+                            </div>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
                 </div>
             </div>
 
@@ -95,7 +112,9 @@
                 <div class="well well-sm text-center">
                     <div class="btn-group-vertical">
                         <br>
-                        <button type="button" class="btn btn-secondary"><a href="<?php echo URL_ROOT ;?>tailors/customers/<?php echo $data['tailor']['tailor_id']; ?>">My Customers</a></button>
+                        <button type="button" class="btn btn-secondary"><a
+                                    href="<?php echo URL_ROOT; ?>tailors/customers/<?php echo $data['tailor']['tailor_id']; ?>">My
+                                Customers</a></button>
                         <br>
                         <button type="button" class="btn btn-secondary">My Orders</button>
                     </div>
